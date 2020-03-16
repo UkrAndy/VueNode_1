f6cfd28ef1598ea88d4b970a70d8cffa57292ee3
UkrAndy <brila_andrij@ukr.net>
2020-03-16 21:12:45 +0200

    vuex

---
+++ 16.Vuex/online_shop/test_pr/src/pages/Home/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-select v-model="viewState">
+      <option v-for="option in viewStates" :value="option" :key="option">{{ option}}</option>
+    </b-select>
+
+    <products-cards v-if="viewState=='cards'" />
+    <products-table v-else />
+    
+  </div>
+</template>
+
+<script>
+import ProductsTable from "./c/ProductsTable";
+import ProductsCards from "./c/ProductsCards";
+
+export default {
+  name: "Home",
+  components: {
+    ProductsTable,
+    ProductsCards
+  },
+
+  data() {
+    return {
+      viewStates: ["cards", "table"],
+      viewState: "cards"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
